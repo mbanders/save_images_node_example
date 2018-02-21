13e3eb702d34846cabd60a0a4e6e6227fc60c2ca
Mike Anderson <michael.anderson@arm.com>
2018-02-21 10:17:18 -0600

    js-beautify everything
    Signed-off-by: Mike Anderson <michael.anderson@arm.com>

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ const port = 80;
 // Limit file upload size to 10 MB
 const max_img_mb = 10;
 app.use(fileUpload({
-    limits: { fileSize: max_img_mb * 1024 * 1024 },
+    limits: {
+        fileSize: max_img_mb * 1024 * 1024
+    },
 }));
 
 app.set('view engine', 'ejs');
@@ -16,18 +18,20 @@ app.set('view engine', 'ejs');
 // Retrieve file
 app.get('/image/:id', (req, res) => {
     mymongo.get_image(req.params.id, (err, doc) => {
-	if (!err && doc) {
+        if (!err && doc) {
             res.contentType(doc.imgType);
             res.send(new Buffer.from(doc.imgData, 'base64'));
-	} else {
-	    res.send();
-	}
+        } else {
+            res.send();
+        }
     });
 });
 
 // Main default page for showing image and uploading a new one
 app.get("/d/:id", (req, res) => {
-    res.render('index', {user_id: req.params.id});
+    res.render('index', {
+        user_id: req.params.id
+    });
 });
 
 
@@ -38,23 +42,26 @@ app.post("/d/:id", (req, res) => {
     }
     // You can include various keys, but
     // the required keys are: _id, imgData, imgType
-    var doc = {firstName: "john",
-	       lastName: "smith",
-	       _id: req.params.id,
-	       imgData: req.files.map.data.toString('base64'),
-	       imgType: req.files.map.mimetype
-	      }
+    var doc = {
+        firstName: "john",
+        lastName: "smith",
+        _id: req.params.id,
+        imgData: req.files.map.data.toString('base64'),
+        imgType: req.files.map.mimetype
+    }
     mymongo.save_image(doc, (err, result) => {
-	if (err) {
-          return res.status(500).send(err);
+        if (err) {
+            return res.status(500).send(err);
         }
-	res.render('index', {user_id: req.params.id});
+        res.render('index', {
+            user_id: req.params.id
+        });
     });
 });
 
 
 // Connect to database and run web server
-mymongo.connect_to_mongo( () =>{
+mymongo.connect_to_mongo(() => {
     app.listen(port, () => {
         console.log("Web server now listening on port " + port);
     });
--- mymongo.js
+++ mymongo.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 
 // Get mongodb host, username, etc
 const config = require("./config");
- 
+
 // Database Name
 const dbName = 'myproject';
 const collectionName = 'images';
@@ -14,23 +14,27 @@ var images = null;
 // Connect to mongodb server
 module.exports.connect_to_mongo = function(callback) {
     MongoClient.connect(config.mongodb_host, (err, client) => {
-	assert.equal(null, err);
-	console.log("Connected successfully to mongo server at " + config.mongodb_host);
-	images = client.db(dbName).collection(collectionName);
+        assert.equal(null, err);
+        console.log("Connected successfully to mongo server at " + config.mongodb_host);
+        images = client.db(dbName).collection(collectionName);
     });
     callback();
 }
 
 // Retrieve the image
 module.exports.get_image = function(id, callback) {
-    images.findOne({"_id": id}, (err, doc) => {
+    images.findOne({
+        "_id": id
+    }, (err, doc) => {
         callback(err, doc);
     });
 }
 
 // Save the image
 module.exports.save_image = function(doc, callback) {
-    images.findOneAndDelete({_id: doc._id}, (err, result) => {
+    images.findOneAndDelete({
+        _id: doc._id
+    }, (err, result) => {
         images.insert(doc, (err, result) => {
             assert.equal(err, null);
             //console.log("Inserted image.");
